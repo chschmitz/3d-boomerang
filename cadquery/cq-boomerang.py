739e79ca766840493a155cdaf92f007f664c13f0
Christoph Schmitz <c.schmitz@hochschule-trier.de>
2026-03-11 14:07:00 +0100

    Refactored FreeCAD model.

--- cadquery/cq-boomerang.py
+++ cadquery/cq-boomerang.py
@@ -15,6 +15,7 @@ FILLET = 2
 
 outline = (
     cq.Sketch()
+    .segment((0, 0), (0, 100), "vertical")
     .arc((-10, -10), (0, 0), (10, -10), "ie")       # inner elbow
     .segment((10, -10), (150, -200), "ri")            # right wing inner
     .arc((150, -200), (170, -220), (190, -200), "rt")  # right wingtip
@@ -44,47 +45,11 @@ outline = (
     .constrain("ie", "Radius", INNER_RADIUS)
     .constrain("oe", "Radius", OUTER_RADIUS)
     .solve()
-    .assemble()
 )
 
-top_outline = (
-    cq.Sketch()
-    .arc((-10, -10), (0, 0), (10, -10), "ie")       # inner elbow
-    .segment((10, -10), (170, -180), "ri")            # right wing inner
-    .arc((170, -180), (180, -220), (190, -200), "rt")  # right wingtip
-    .arc((190, -200), (90, -100), (50, -10), "ro")    # right wing outer
-    .arc((50, -10), (0, 50), (-50, -10), "oe")      # outer elbow
-    .arc((-50, -10), (-90, -100), (-200, -210), "lo") # left wing outer
-    .arc((-200, -210), (-180, -230), (-130, -210), "lt") # left wingtip
-    .segment((-130, -210), (-10, -10), "li")   # left wing inner
-    .constrain("ie", "ri", "Coincident", None)
-    .constrain("ri", "rt", "Coincident", None)
-    .constrain("rt", "ro", "Coincident", None)
-    .constrain("ro", "oe", "Coincident", None)
-    .constrain("oe", "lo", "Coincident", None)
-    .constrain("lo", "lt", "Coincident", None)
-    .constrain("lt", "li", "Coincident", None)
-    .constrain("li", "ie", "Coincident", None)
-    .constrain("ie", "ri", "Angle", 0)
-    .constrain("ri", "rt", "Angle", 0)
-    .constrain("rt", "ro", "Angle", 0)
-    .constrain("ro", "oe", "Angle", 0)
-    .constrain("oe", "lo", "Angle", 0)
-    .constrain("lo", "lt", "Angle", 0)
-    .constrain("lt", "li", "Angle", 0)
-    .constrain("li", "ie", "Angle", 0)
-    .constrain("lt", "Radius", TOP_TIP_RADIUS)
-    .constrain("rt", "Radius", TOP_TIP_RADIUS)
-    .constrain("ie", "Radius", TOP_INNER_RADIUS)
-    .constrain("oe", "Radius", TOP_OUTER_RADIUS)
-    .solve()
-    .assemble()
-)
+outline.assemble()
 
-top = (cq.Workplane()
-     .placeSketch(outline.moved(cq.Location(cq.Vector(0, 0, TRAILING_EDGE))), 
-                  top_outline.moved(cq.Location(cq.Vector(0, 0, THICKNESS))))
-     .loft().faces("+Z").fillet(FILLET))
-bottom = (cq.Workplane().placeSketch(outline).extrude(TRAILING_EDGE))
-result = top.union(bottom)
+ri = outline._tags["ri"][0]
 
+print(ri.startPoint())
+print(ri.endPoint())
